f02c53b355cc9503aef1cdda171180b9ca213a7c
TimurBazarov <74816879+TimurBazarov@users.noreply.github.com>
2023-04-08 14:35:22 +0300

    optimized text detecting and reading

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from text_detecting import detect_and_open_text
 
 
-detect_and_open_text('img1.png')
-detect_and_open_text('img2.png')
-detect_and_open_text('img3.png')
+def test():
+    detect_and_open_text('img1.png')
+    detect_and_open_text('img2.png')
+    detect_and_open_text('img3.png')
+
+
+test()
--- text_detecting.py
+++ text_detecting.py
@@ -16,21 +16,20 @@ def detect_and_open_text(filename):
     image = cv2.cvtColor(cv2.imread(PATH_TO_IMAGE + filename), cv2.COLOR_BGR2RGB)
     sh = image.shape
     img = image.copy()
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     par0, par1 = blur_par(sh[0]), blur_par(sh[1])
 
-    #  bluring
     img = cv2.GaussianBlur(img, (par0, par1), 0)
 
-    #  thresholding
-    _, img = cv2.threshold(img, 80, 200, cv2.ADAPTIVE_THRESH_MEAN_C)
+    # _, img = cv2.threshold(img, 80, 200, cv2.ADAPTIVE_THRESH_MEAN_C)
 
     #  reading
     text = reader.readtext(img)
     for t in text:
         box, text, score = t
-        cv2.rectangle(image, [int(i) for i in box[0]], [int(i) for i in box[2]], (255, 255, 0))
-        cv2.putText(image, text, [int(i) for i in box[0]], cv2.FONT_HERSHEY_COMPLEX, 1, (225, 0, 0), 2)
+        cv2.rectangle(img, [int(i) for i in box[0]], [int(i) for i in box[2]], (255, 255, 0))
+        cv2.putText(img, text, [int(i) for i in box[0]], cv2.FONT_HERSHEY_COMPLEX, 1, (225, 0, 0), 2)
 
     # output
-    pl.imshow(image)
+    pl.imshow(img)
     pl.show()
